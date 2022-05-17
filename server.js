2e7711501958e5048cca7172f289d66df38c6c15
Brendan Beck <brendan.beck62@gmail.com>
2022-05-16 17:52:41 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config({path: __dirname + `/.env.${process.env.NODE_ENV}`});
+const express = require('express');
+const app = express();
+const fs = require('fs')
+
+//const mongoose = require('mongoose');
+//require('./routes/sneaks.routes.js')(app);
+//const SneaksAPI = require('./controllers/sneaks.controllers.js');
+
+var HOST = process.env.HOST || '127.0.0.1';
+var PORT = process.env.PORT || 8080;
+//mongoose.Promise = global.Promise;
+
+app.get('/', (req, res) => {
+  res.end('Hello World!');
+});
+
+app.get("/list_movies", (req, res) => {
+    fs.readFile(__dirname + '/' + 'movies.json', 'utf8', (err, data) => {
+        res.end(data);
+    });
+});
+
+app.listen(PORT, function () {
+  console.log(`stonkb-api listening on port`, PORT);
+ });
+
+
+//module.exports = app;
+//module.exports = SneaksAPI;
